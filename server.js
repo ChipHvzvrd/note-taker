710f6eb82539893e4776313113e65d4d8818c102
chiphvzvrd <christianipena@yahoo.com>
2022-10-11 22:24:57 -0500

    new routes

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ const app = express();
 
 const { note } = require('./public/models/Note');
 
-app.get('/api/Note', (req, res) => {
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
+
+app.get('/api/notes', (req, res) => {
     res.send('Some Data');
+})
+
+app.get('/api/notes/id:', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 })
 
 app.listen(PORT, ()=> {
