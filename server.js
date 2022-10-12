9c148776866de9aa7dc8be519234b1d46c1c1742
chiphvzvrd <christianipena@yahoo.com>
2022-10-11 23:22:47 -0500

    POST route created

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 const { notes } = require('./public/data/notes');
 
 function filterByQuery(query, notesArray) {
@@ -18,6 +22,16 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 };
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+};
+
+function createNewNote(body, notesArray) {
+    console.log(body);
+    return body;
+};
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
@@ -26,13 +40,18 @@ app.get('/api/notes', (req, res) => {
     res.json(results)
 });
 
-app.get('/api/notes/id:', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if(result) {
         res.json(result);
     } else {
         res.send(404);
     }
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, ()=> {
